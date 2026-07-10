2ea1ad48b838adf484bda16c6be38ef3877ef7e3
santiago-reyes-buitrago <santiagoreyesbuitrago02@gmail.com>
2026-07-10 09:07:36 -0500

    feat(graphQl-section-5): initial project in section 5

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import {join} from "path";
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
