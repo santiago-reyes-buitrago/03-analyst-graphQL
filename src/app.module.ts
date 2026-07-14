8429771995cd3ccbfdbe166e39ec86120599113f
santiago-reyes-buitrago <santiagoreyesbuitrago02@gmail.com>
2026-07-14 09:08:31 -0500

    feat(graphQl-section-5): create connection for bd and create entity items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,35 @@
-import { Module } from '@nestjs/common';
+import {join} from "path";
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import {GraphQLModule} from "@nestjs/graphql";
 import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
-import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
-import {join} from "path";
-import { ItemsModule } from './items/items.module';
+import {ApolloServerPluginLandingPageLocalDefault} from "@apollo/server/plugin/landingPage/default";
+import {ItemsModule} from './items/items.module';
+
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
-      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     ItemsModule,
@@ -18,4 +37,5 @@ import { ItemsModule } from './items/items.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
